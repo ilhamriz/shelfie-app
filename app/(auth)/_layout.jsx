4e8de2911614a1c0c2a3cf3defb9a0e6c5f3e20f
ilhamriz <milhamrizky7@gmail.com>
2025-05-31 17:17:32 +0700

    Protecting Routes

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
+import { useUser } from "../../hooks/useUser";
+import GuestOnly from "../../components/auth/GuestOnly";
 
 const AuthLayout = () => {
+  const { user } = useUser();
+  console.log("user =>", user);
+
   return (
-    <>
+    <GuestOnly>
       <StatusBar style="auto" />
       <Stack screenOptions={{ headerShown: false, animation: "none" }} />
-    </>
+    </GuestOnly>
   );
 };
 
---
+++ components/auth/UserOnly.jsx
@@ -0,0 +1,19 @@
+import { Text } from "react-native";
+import { useUser } from "../../hooks/useUser";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+
+const UserOnly = ({ children }) => {
+  const { user, authChecked } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (authChecked && user === null) {
+      router.replace("/login");
+    }
+  }, [user, authChecked]);
+
+  return authChecked || user ? children : <Text>Loading</Text>;
+};
+
+export default UserOnly;
