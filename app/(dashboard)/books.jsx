ad7954bd848867981ab61ed06f3f35b90dcdfefb
ilhamriz <milhamrizky7@gmail.com>
2025-06-02 21:30:34 +0700

    Using FlatList in Books Page

--- app/(dashboard)/books.jsx
+++ app/(dashboard)/books.jsx
@@ -1,13 +1,32 @@
-import { StyleSheet } from "react-native";
+import { FlatList, Pressable, StyleSheet } from "react-native";
+import { useBooks } from "../../contexts/BooksContext";
+import { Colors } from "../../constants/Colors";
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
+import ThemedCard from "../../components/ThemedCard";
 
 const Books = () => {
+  const { books } = useBooks();
+
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView style={styles.container} isSafeArea>
       <ThemedText title style={styles.heading}>
         Your Reading List
       </ThemedText>
+
+      <FlatList
+        data={books}
+        keyExtractor={(item) => item.$id}
+        contentContainerStyle={styles.list}
+        renderItem={({ item }) => (
+          <Pressable>
+            <ThemedCard style={styles.card}>
+              <ThemedText style={styles.title}>{item.title}</ThemedText>
+              <ThemedText>Written by {item.author}</ThemedText>
+            </ThemedCard>
+          </Pressable>
+        )}
+      />
     </ThemedView>
   );
 };
@@ -24,5 +43,20 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 18,
     textAlign: "center",
+  },
+  list: { marginTop: 40 },
+  card: {
+    width: "90%",
+    marginHorizontal: "5%",
+    marginVertical: 10,
+    padding: 10,
+    paddingLeft: 14,
+    borderLeftWidth: 4,
+    borderLeftColor: Colors.primary,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
 });
